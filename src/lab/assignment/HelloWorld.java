bf575097633eb8b650766d292f1480f5156ed596
Eldiiar <55808560+eldiiarchormonov@users.noreply.github.com>
2022-08-05 10:48:40 -0500

    Delete HelloWorld.java

--- src/lab/assignment/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package lab.assignment;
-
-public class HelloWorld {
-    public static void main(String[]args){
-        System.out.println("Hello World");
-        System.out.println("Java Progrmming is easy.");
-    }
-}
